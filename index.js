49bf52fb703c27da8bf44b975ac525dc45c4bf6a
Hassan Razak <hassanrazak1991@gmail.com>
2022-11-23 09:34:53 -0500

    commit tree 1f88ce8829925cc9af52efbb105dfe7734ea6052

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const hqLoc = 42;
+
+function distanceFromHqInBlocks(arg){
+    return Math.abs(arg - hqLoc);
+
+};
+
+
+function distanceFromHqInFeet(arg){
+    return distanceFromHqInBlocks(arg)*264
+}
+
+function distanceTravelledInFeet(arg1,arg2){
+    return Math.abs(arg1-arg2)*264;
+
+}
+
+function calculatesFarePrice(start, destination){
+    let x = (distanceTravelledInFeet(start,destination));
+    debugger;
+    switch(x){
+        
+        case (x <= 400):
+            x = 0
+            break;
+        case (x >400 && x < 2000):
+           x = x*.02;
+           break;
+        case (x > 2000 && x < 2500):
+            x = 25
+            break;
+        case (x > 2500):
+            x = "not allowed to ride";
+            break;
+    }
+
+    return x;
+}
